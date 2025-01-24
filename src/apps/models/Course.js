9c83795870e288beea3960db299266918750f9f1
hiengianguyen <hiennguyen2060@gmail.com>
2025-01-24 21:51:28 +0700

    create course

--- src/apps/models/Course.js
+++ src/apps/models/Course.js
@@ -1,18 +1,21 @@
-const mongoose = require('mongoose');  
-const Schema = mongoose.Schema;  
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-// const slug = require('mongoose-slug-generator');
+const slug = require("mongoose-slug-generator");
 
-// mongoose.plugin(slug);
+mongoose.plugin(slug);
 
-const Course = new Schema({  
-    name: { type: String, required: true },  
-    description: { type: String },  
-    image: { type: String },  
-    videoId: { type: String, required: true },  
-    // slug: { type: String, slug: 'name', unique: true }
-}, {
+const Course = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String },
+    image: { type: String },
+    videoId: { type: String, required: true },
+    slug: { type: String, slug: "name", unique: true },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-module.exports = mongoose.model('Course', Course);
+module.exports = mongoose.model("Course", Course);
