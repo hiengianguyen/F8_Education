29b3812ea6cbe76aee79ed56c57ab1e6ddeb5e16
hiengianguyen <hiennguyen2060@gmail.com>
2025-02-16 21:36:30 +0700

    Change uri "string" to "env variable"

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/f8_education_dev", {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
